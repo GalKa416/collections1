c8f4bb0b6f632c18c36ba470c6927d9eae9a9214
Галина <g416@mail.ru>
2023-07-02 14:31:51 +0500

    дз 2.7 готова

--- src/main/java/com/example/employeewebadd/exeption/EmployeeNotFoundExeption.java
+++ src/main/java/com/example/employeewebadd/exeption/EmployeeNotFoundExeption.java
@@ -3,7 +3,7 @@ package com.example.employeewebadd.exeption;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus (code = HttpStatus.BAD_REQUEST)
+@ResponseStatus (code = HttpStatus.NOT_FOUND)
 
 public class EmployeeNotFoundExeption extends RuntimeException{
     public EmployeeNotFoundExeption() {
